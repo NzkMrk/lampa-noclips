a6f867f06ba8697056b89dc22458598182aa8978
NzkMrk <149791746+NzkMrk@users.noreply.github.com>
2026-02-02 16:34:10 +0100

    Update uakino.js

--- uakino.js
+++ uakino.js
@@ -1,72 +1,42 @@
 (function () {
     'use strict';
 
-    function UAOnline(object) {
-        var network = new Lampa.Reguest();
-        var scroll  = new Lampa.Scroll({mask: true, over: true});
-        var items   = [];
-        
-        this.create = function () {
-            var _this = this;
-            var query = object.search || (object.movie ? (object.movie.title || object.movie.name) : '');
-            
-            // Створюємо порожній контейнер
-            var html = $('<div><div class="wait" style="padding: 20px;">Пошук українською для: ' + query + '...</div></div>');
-            
-            // Додаємо в скролл
-            scroll.append(html);
-            
-            // Тут буде логіка парсингу пізніше
-            setTimeout(function(){
-                html.find('.wait').text('Розділ у розробці (Пошук: ' + query + ')');
-            }, 1500);
-
-            return scroll.render();
-        };
-    }
-
     function startPlugin() {
-        console.log('UAkino: Plugin started');
+        // 1. Виводимо повідомлення в кутку екрана
+        Lampa.Noty.show('Тестовий плагін активовано!');
 
-        // Функція додавання кнопки
-        var addBtn = function(e) {
-            if ($('.full-start__buttons .view--ua-online').length > 0) return;
+        // 2. Додаємо пункт у головне меню
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') {
+                var menu_item = {
+                    title: 'Тест UAkino',
+                    icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" fill="white"/></svg>',
+                    id: 'test_ua_plugin'
+                };
 
-            var btn = $('<div class="full-start__button selector view--ua-online" style="background: #24b353 !important; color: #fff !important; border-radius: 5px; margin-right: 10px;"><svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" style="fill: white; vertical-align: middle; margin-right: 5px;"><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg><span>UA Online</span></div>');
-
-            btn.on('hover:enter', function () {
-                Lampa.Activity.push({
-                    url: '',
-                    title: 'UA Online',
-                    component: 'ua_online',
-                    movie: e.data.movie,
-                    page: 1
-                });
-            });
-
-            $('.full-start__buttons').prepend(btn);
-        };
-
-        // Слухаємо відкриття картки
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
-                addBtn(e);
+                // Додаємо в список меню
+                Lampa.Menu.add(menu_item);
             }
         });
 
-        // Реєструємо компонент
-        Lampa.Component.add('ua_online', UAOnline);
+        // 3. Обробка натискання на цей пункт
+        Lampa.Component.add('test_ua_component', function (object) {
+            var comp = new Lampa.Interaction();
+            
+            comp.create = function () {
+                return '<div><h1 style="padding: 20px;">Вітаю! Плагін працює.</h1></div>';
+            };
+            
+            return comp;
+        });
     }
 
-    // Запуск з перевіркою оточення
-    try {
-        if (window.appready) startPlugin();
-        else {
-            Lampa.Listener.follow('app', function (e) {
-                if (e.type == 'ready') startPlugin();
-            });
-        }
-    } catch (err) {
-        console.error("UAkino Error: ", err);
+    // Реєстрація плагіна
+    if (window.appready) {
+        startPlugin();
+    } else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') startPlugin();
+        });
     }
 })();
