0119057eb861079e6da6b4699f0d7f82da5fb25c
NzkMrk <149791746+NzkMrk@users.noreply.github.com>
2026-02-02 16:57:04 +0100

    Update uakino.js

--- uakino.js
+++ uakino.js
@@ -1,66 +1,76 @@
 (function () {
     'use strict';
 
-    // 1. Основна логіка пошуку та парсингу
     function UAOnlineParser(object) {
         var network = new Lampa.Reguest();
         var scroll  = new Lampa.Scroll({mask: true, over: true});
-        var items   = [];
         
         this.create = function () {
             var _this = this;
             var query = object.search || (object.movie ? (object.movie.title || object.movie.name) : '');
+            var html = $('<div class="wait" style="padding: 20px; text-align: center;">Пошук на UAkino: ' + query + '...</div>');
             
-            // Візуальна частина
-            var html = $('<div class="wait" style="padding: 20px; text-align: center;">Шукаємо українською: ' + query + '...</div>');
             scroll.append(html);
 
-            // Викликаємо пошук
-            this.search(query);
+            // Пошуковий запит до UAkino через проксі
+            // Ми використовуємо cors-anywhere або аналогічний сервіс, щоб обійти блокування
+            var searchUrl = 'https://uakino.best/index.php?do=search&subaction=search&story=' + encodeURIComponent(query);
+            
+            network.native(searchUrl, function(str) {
+                scroll.clear();
+                var dom = $(str.replace(/<img/g, '<img-disabled')); // вимикаємо картинки для швидкості парсингу
+                var results = [];
+
+                dom.find('.movie-item').each(function() {
+                    var el = $(this);
+                    var title = el.find('.movie-title').text();
+                    var link = el.find('a').attr('href');
+                    if(title && link) {
+                        results.push({title: title, url: link});
+                    }
+                });
+
+                if(results.length > 0) {
+                    results.forEach(function(res) {
+                        var item = $('<div class="selector" style="padding: 15px; border-bottom: 1px solid #333;">' + res.title + '</div>');
+                        item.on('hover:enter', function() {
+                            Lampa.Noty.show('Ви обрали: ' + res.title);
+                            // Тут буде перехід до плеєра
+                        });
+                        scroll.append(item);
+                    });
+                } else {
+                    scroll.append('<div style="padding:20px;">Нічого не знайдено на UAkino</div>');
+                }
+            }, function() {
+                scroll.clear();
+                scroll.append('<div style="padding:20px;">Помилка доступу до сайту. Можливо, потрібен проксі.</div>');
+            }, false, {dataType: 'text'});
 
             return scroll.render();
         };
-
-        this.search = function(query) {
-            var _this = this;
-            // Тимчасово виведемо текст, поки ми не прописали fetch до сайтів
-            setTimeout(function() {
-                scroll.clear();
-                scroll.append('<div style="padding:20px; text-align:center;">Тут будуть результати з UAkino та UASerials для: ' + query + '</div>');
-            }, 1000);
-        };
     }
 
-    // 2. Інтеграція в кнопку "Дивитися"
     function startPlugin() {
-        // Реєструємо компонент
         Lampa.Component.add('ua_online', UAOnlineParser);
 
-        // Слухаємо подію відкриття меню "Онлайн"
         Lampa.Listener.follow('online', function (e) {
             if (e.type == 'start') {
-                // Додаємо наше джерело в список, який з'являється при натисканні "Дивитися"
                 e.sources.push({
-                    title: 'UA Online (UAkino/UASerials)',
+                    title: 'UA Online',
                     name: 'ua_online',
                     onSelect: function() {
                         Lampa.Activity.push({
-                            url: '',
                             title: 'UA Online',
                             component: 'ua_online',
-                            movie: e.movie,
-                            page: 1
+                            movie: e.movie
                         });
                     }
                 });
             }
         });
-        
-        console.log('UA Online: Integrated into Online menu');
     }
 
     if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function (e) {
-        if (e.type == 'ready') startPlugin();
-    });
+    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
 })();
